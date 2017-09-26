d9fbedb857c566a47079ac79bcf3f1c55a80f761
Fransy <fransyrcc@gmail.com>
2017-09-26 19:39:19 +0100

    Class based components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react'; // Import React and pull off the property Component as a varaible called component
+// Same as saying:
+// const Component = React.Component
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
     render() {
         return <input />;
     }
-
 }
 
 export default SearchBar;
