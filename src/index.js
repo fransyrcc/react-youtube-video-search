fc3e0fc182ba1f3eff677e2f32f7a949801acc4d
Fransy <fransyrcc@gmail.com>
2017-09-26 19:28:38 +0100

    export statements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'; // Knows how to work with react components
 import ReactDOM from 'react-dom'; // Separate library to render in DOM
 
+import SearchBar from './components/search_bar'; 
+
+const API_KEY = 'AIzaSyBSgQJZxZyaxppVghptlXUoXkS9NB59gCk';
+
 // Create mew component. This component should product some HTML
 const App = () => {
-       return <div>Hi!</div>;
+       return (
+       <div>
+           <SearchBar />
+       </div>
+       );
 }
 
 // Take this copononent/s generated HTML and put it on onthe page (in the DOM)
