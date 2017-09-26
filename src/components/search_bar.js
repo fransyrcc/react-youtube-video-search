20f3c5b19e33475629f2e0435b4d289d2cf5ca25
Fransy <fransyrcc@gmail.com>
2017-09-26 20:56:44 +0100

    referencing the value of state term to show it

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,7 +6,13 @@ class SearchBar extends Component {
         this.state = { term: '' };
     }
     render() {
-        return <input onChange={event => console.log(event.target.value)} />;
+        return (
+            <div>
+                <input onChange={event => this.setState({ term: event.target.value })} />
+                <p>Value of the input: <br />
+                {this.state.term}</p>
+            </div>
+        );
     }
 }
 
