fc2482282790945b58857408bc02b34950d78c12
Fransy <fransyrcc@gmail.com>
2017-09-27 22:06:53 +0100

    Ajax request with React

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom'; 
-
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar'; 
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBSgQJZxZyaxppVghptlXUoXkS9NB59gCk';
 
-YTSearch({    key: API_KEY,    term: 'surfboards'},    function(data) {
-        console.log(data);
-});
-// Create mew component. This component should product some HTML
-const App = () => {
-       return (
-       <div>
-           <SearchBar />
-       </div>
-       );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+        YTSearch({ key: API_KEY, term: 'surfboards'}, videos => {
+            this.setState({ videos }); // this.setState({ videos: videos }           
+        }); 
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} /> 
+                <VideoList videos={this.state.videos} /> 
+            </div>
+        );
+    }
 }
 
-// Take this copononent/s generated HTML and put it on onthe page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
